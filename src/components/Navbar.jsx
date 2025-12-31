4e474b2d4a5e6cb1b95650bd250dceef5ad6941b
rajesh-atpar <rajesh@atparui.com>
2025-12-31 21:00:25 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav style={{ padding: "1rem", borderBottom: "1px solid #ccc" }}>
+      <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
+      <Link to="/about" style={{ marginRight: "1rem" }}>About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+};
+
+export default Navbar;
